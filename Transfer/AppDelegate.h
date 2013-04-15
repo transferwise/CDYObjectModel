2865326439e046e8504edffc6cbd083bfcece0d4
Jaanus Siim <jaanus@jaanussiim.com>
2013-04-15 08:37:25 +0300

    Initial project files

---
+++ Transfer/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Transfer
+//
+//  Created by Jaanus Siim on 4/15/13.
+//  Copyright (c) 2013 Mooncascade OÜ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Transfer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Transfer
+//
+//  Created by Jaanus Siim on 4/15/13.
+//  Copyright (c) 2013 Mooncascade OÜ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
