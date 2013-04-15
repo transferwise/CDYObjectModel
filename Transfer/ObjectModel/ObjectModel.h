7803d53690dcb8be5f8ef8db89a776faeff7ca65
Jaanus Siim <jaanus@jaanussiim.com>
2013-04-15 09:23:41 +0300

    Move CoreData code to ObjectModel

---
+++ Transfer/ObjectModel/ObjectModel.h
@@ -0,0 +1,15 @@
+//
+//  ObjectModel.h
+//  Transfer
+//
+//  Created by Jaanus Siim on 4/15/13.
+//  Copyright (c) 2013 Mooncascade OÜ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ObjectModel : NSObject
+
+- (void)saveContext;
+
+@end
