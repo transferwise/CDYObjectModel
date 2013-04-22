c4618d4e915e1b3989ac0559ea81f966c1ab94ad
Jaanus Siim <jaanus@jaanussiim.com>
2013-04-22 12:19:59 +0300

    Base for showing currency picker

--- Transfer/Cells/MoneyEntryCell.m
+++ Transfer/Cells/MoneyEntryCell.m
@@ -10,13 +10,11 @@
 
 NSString *const TWMoneyEntryCellIdentifier = @"TWMoneyEntryCell";
 
-@interface MoneyEntryCell () <UITextFieldDelegate>
+@interface MoneyEntryCell () <UITextFieldDelegate, UIPickerViewDataSource, UIPickerViewDelegate>
 
 @property (nonatomic, strong) IBOutlet UILabel *titleLabel;
 @property (nonatomic, strong) IBOutlet UITextField *moneyField;
 @property (nonatomic, strong) IBOutlet UITextField *currencyField;
-
-- (IBAction)changeCurrencyTapped:(id)sender;
 
 @end
 
@@ -43,10 +41,18 @@ NSString *const TWMoneyEntryCellIdentifier = @"TWMoneyEntryCell";
 
     //TODO jaanus: maybe can find a way how to change keyboard locale
     [self.moneyField setDelegate:self];
+
+    [self.currencyField setInputAccessoryView:toolbar];
+    UIPickerView *picker = [[UIPickerView alloc] initWithFrame:CGRectZero];
+    [picker setShowsSelectionIndicator:YES];
+    [self.currencyField setInputView:picker];
+    [picker setDataSource:self];
+    [picker setDelegate:self];
 }
 
 - (void)donePressed {
     [self.moneyField resignFirstResponder];
+    [self.currencyField resignFirstResponder];
 }
 
 - (void)setTitle:(NSString *)title {
@@ -66,10 +72,6 @@ NSString *const TWMoneyEntryCellIdentifier = @"TWMoneyEntryCell";
     return [self.currencyField text];
 }
 
-- (IBAction)changeCurrencyTapped:(id)sender {
-
-}
-
 - (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
     if (![string isEqualToString:@","]) {
         return YES;
@@ -80,6 +82,22 @@ NSString *const TWMoneyEntryCellIdentifier = @"TWMoneyEntryCell";
     }
 
     return NO;
+}
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return 2;
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    if (row == 0) {
+        return @"EUR";
+    } else {
+        return @"GBP";
+    }
 }
 
 @end
---
+++ Transfer/Views/NonEditTextField.h
@@ -0,0 +1,13 @@
+//
+//  NonEditTextField.h
+//  Transfer
+//
+//  Created by Jaanus Siim on 4/22/13.
+//  Copyright (c) 2013 Mooncascade OÜ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NonEditTextField : UITextField
+
+@end
