b4343748fb405fea127222264d7e75fcae2f6745
Henri Mägi <henri@henris-iMac.local>
2013-06-17 16:55:26 +0300

    TransferType selection stuff, to get rid of the radiobuttons.

---
+++ Transfer/Cells/TransferTypeSelectionCell.h
@@ -0,0 +1,15 @@
+//
+//  TransferTypeSelectionCell.h
+//  Transfer
+//
+//  Created by Henri Mägi on 17.06.13.
+//  Copyright (c) 2013 Mooncascade OÜ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TransferTypeSelectionCell : UITableViewCell
+@property (strong, nonatomic) IBOutlet UILabel *leftLabel;
+@property (strong, nonatomic) IBOutlet UILabel *rightLabel;
+
+@end
---
+++ Transfer/Cells/TransferTypeSelectionCell.m
@@ -0,0 +1,29 @@
+//
+//  TransferTypeSelectionCell.m
+//  Transfer
+//
+//  Created by Henri Mägi on 17.06.13.
+//  Copyright (c) 2013 Mooncascade OÜ. All rights reserved.
+//
+
+#import "TransferTypeSelectionCell.h"
+
+@implementation TransferTypeSelectionCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
