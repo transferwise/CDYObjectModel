c39416b949360212ffd61679d23bbc91afa0e3d6
Jaanus Siim <jaanus@jaanussiim.com>
2013-10-15 13:07:42 +0300

    Re #149 added configuration options request

---
+++ Transfer/Network/ConfigurationOptionsOperation.h
@@ -0,0 +1,13 @@
+//
+//  ConfigurationOptionsOperation.h
+//  Transfer
+//
+//  Created by Jaanus Siim on 10/15/13.
+//  Copyright (c) 2013 Mooncascade OÜ. All rights reserved.
+//
+
+#import "TransferwiseOperation.h"
+
+@interface ConfigurationOptionsOperation : TransferwiseOperation
+
+@end
---
+++ Transfer/Network/ConfigurationOptionsOperation.m
@@ -0,0 +1,13 @@
+//
+//  ConfigurationOptionsOperation.m
+//  Transfer
+//
+//  Created by Jaanus Siim on 10/15/13.
+//  Copyright (c) 2013 Mooncascade OÜ. All rights reserved.
+//
+
+#import "ConfigurationOptionsOperation.h"
+
+@implementation ConfigurationOptionsOperation
+
+@end
