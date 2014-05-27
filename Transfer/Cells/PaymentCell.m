53cd3e5bde900de41cafd2656e261cd2c1d0fae8
Mats Trovik <mats@matsomatic.co.uk>
2014-05-27 14:24:16 +0100

    https://www.pivotaltracker.com/story/show/71562758 Revert accidental commit of test code.

--- Transfer/Cells/PaymentCell.m
+++ Transfer/Cells/PaymentCell.m
@@ -33,7 +33,7 @@
     [self.currencyLabel setText:[payment transferredCurrenciesString]];
     
     UIImage *icon;
-    switch (PaymentStatusRefunded){//[payment status]) {
+    switch ([payment status]) {
         case PaymentStatusCancelled:
             icon = [UIImage imageNamed:@"transfers_icon_cancelled"];
             break;
