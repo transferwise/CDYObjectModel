0b194b0943473095d8b603ceba386cddd332c789
Mats Trovik <mats@matsomatic.co.uk>
2014-05-28 17:24:19 +0100

    https://www.pivotaltracker.com/story/show/71560394 fixed truncated LOGIN text.

---
+++ Transfer/Views/InsetRespectingButton.m
@@ -0,0 +1,31 @@
+//
+//  InsetRespectingButton.m
+//  Transfer
+//
+//  Created by Mats Trovik on 28/05/2014.
+//  Copyright (c) 2014 Mooncascade OÜ. All rights reserved.
+//
+
+#import "InsetRespectingButton.h"
+
+@implementation InsetRespectingButton
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
