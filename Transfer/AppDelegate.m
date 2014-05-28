94380e63cb1b91b3d2e561553d209e22838150a7
Mats Trovik <mats@matsomatic.co.uk>
2014-05-28 12:47:40 +0100

    https://www.pivotaltracker.com/story/show/71562758 Transfer screen layout tweaks

--- Transfer/AppDelegate.m
+++ Transfer/AppDelegate.m
@@ -87,6 +87,7 @@
     [[UINavigationBar appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName : [navFontStyle color], NSFontAttributeName : [navFontStyle font]}];
     [[UINavigationBar appearance] setBarTintColor:[UIColor colorFromStyle:@"navBarBlue"]];
     [[UINavigationBar appearance] setShadowImage:[[UIImage alloc] init]];
+    [[UINavigationBar appearance] setTitleVerticalPositionAdjustment:-2 forBarMetrics:UIBarMetricsDefault];
 
     
 
