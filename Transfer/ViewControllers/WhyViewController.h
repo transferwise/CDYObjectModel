43aa180110a4d0e5ae04bb3fcc92b409cec0f0ca
Mats Trovik <matstrovik@windows-phone.albionlondon.local>
2014-06-12 17:22:21 +0100

    https://www.pivotaltracker.com/story/show/71560918 Starting styling

--- Transfer/ViewControllers/WhyViewController.h
+++ Transfer/ViewControllers/WhyViewController.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2014 Mooncascade OÜ. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "TransparentModalViewController.h"
 
-@interface WhyViewController : UIViewController
+@interface WhyViewController : TransparentModalViewController
 
 @end
