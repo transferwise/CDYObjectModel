002757376aa2e5f66e29ade451ccd2e6dfaec525
Juhan Hion <juhan@transferwise.com>
2014-06-13 15:13:58 +0100

    https://www.pivotaltracker.com/story/show/71562576 Contact support button complete (except correct color).

--- Transfer/Views/LightGrayButton.m
+++ Transfer/Views/LightGrayButton.m
@@ -31,9 +31,13 @@
 -(void)commonSetup
 {
     [self setTitleColor:[UIColor colorFromStyle:@"lighterBlue"] forState:UIControlStateNormal];
-    [self.titleLabel setFont:[UIFont fontFromStyle:@"heavy.@17"]];
+    [self.titleLabel setFont:[UIFont fontFromStyle:@"medium.@17"]];
 	[self setBackgroundImage:[UIImage imageFromColor:[UIColor colorFromStyle:@"cellSelected"]] forState:UIControlStateNormal];
     self.exclusiveTouch = YES;
+	self.layer.borderWidth = 1.f;
+	self.layer.cornerRadius = 2.f;
+	self.layer.masksToBounds = YES;
+	self.layer.borderColor = [[UIColor colorFromStyle:@"lightGray"] CGColor];
 }
 
 @end
