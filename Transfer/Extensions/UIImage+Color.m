6f9ddbf652c4677f2b5449e7468b98f8b2306ad9
Juhan Hion <juhan@transferwise.com>
2014-06-13 14:39:08 +0100

    https://www.pivotaltracker.com/story/show/71562576 Contact support button added, almost looking nice.

---
+++ Transfer/Extensions/UIImage+Color.m
@@ -0,0 +1,28 @@
+//
+//  UIImage+Color.m
+//  Transfer
+//
+//  Created by Juhan Hion on 13.06.14.
+//  Copyright (c) 2014 Mooncascade OÜ. All rights reserved.
+//
+
+#import "UIImage+Color.h"
+
+@implementation UIImage (Color)
+
++ (UIImage *)imageFromColor:(UIColor *)color
+{
+	CGRect rect = CGRectMake(0.0f, 0.0f, 1.0f, 1.0f);
+	UIGraphicsBeginImageContext(rect.size);
+	CGContextRef context = UIGraphicsGetCurrentContext();
+	
+	CGContextSetFillColorWithColor(context, [color CGColor]);
+	CGContextFillRect(context, rect);
+	
+	UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+	UIGraphicsEndImageContext();
+	
+	return image;
+}
+
+@end
---
+++ Transfer/Views/LightGrayButton.h
@@ -0,0 +1,13 @@
+//
+//  LightGrayButton.h
+//  Transfer
+//
+//  Created by Juhan Hion on 13.06.14.
+//  Copyright (c) 2014 Mooncascade OÜ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LightGrayButton : UIButton
+
+@end
