c20227259da98a1fef92954b20868b6ab84d3d82
Mats Trovik <matstrovik@Matss-MacBook-Pro.local>
2014-06-13 09:29:30 +0100

    https://www.pivotaltracker.com/story/show/71560918 setting up outlets

--- Transfer/ViewControllers/WhyViewController.m
+++ Transfer/ViewControllers/WhyViewController.m
@@ -10,6 +10,24 @@
 
 @interface WhyViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+
+@property (weak, nonatomic) IBOutlet UIImageView *leftImage;
+@property (weak, nonatomic) IBOutlet UILabel *leftRateValue;
+@property (weak, nonatomic) IBOutlet UILabel *leftDescription1;
+@property (weak, nonatomic) IBOutlet UILabel *leftFeeValue;
+@property (weak, nonatomic) IBOutlet UILabel *leftDescription2;
+
+@property (weak, nonatomic) IBOutlet UIImageView *rightImage;
+@property (weak, nonatomic) IBOutlet UILabel *rightRateValue;
+@property (weak, nonatomic) IBOutlet UILabel *rightDescription1;
+@property (weak, nonatomic) IBOutlet UILabel *rightFeeValue;
+@property (weak, nonatomic) IBOutlet UILabel *rightDescription2;
+
+@property (weak, nonatomic) IBOutlet UILabel *vsTitle;
+@property (weak, nonatomic) IBOutlet UILabel *rateTitle;
+@property (weak, nonatomic) IBOutlet UILabel *feeTitle;
+
 @end
 
 @implementation WhyViewController
