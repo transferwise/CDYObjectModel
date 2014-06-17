ca2f93556747324259b216abf8f618751c03f07f
Mats Trovik <yhello@matsomatic.co.uk>
2014-06-17 17:37:12 +0100

    https://www.pivotaltracker.com/story/show/71561276 New green button with progress.

---
+++ Transfer/Views/ProgressButton.m
@@ -0,0 +1,31 @@
+//
+//  ProgressButton.m
+//  Transfer
+//
+//  Created by Mats Trovik on 17/06/2014.
+//  Copyright (c) 2014 Mooncascade OÜ. All rights reserved.
+//
+
+#import "ProgressButton.h"
+
+@implementation ProgressButton
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
