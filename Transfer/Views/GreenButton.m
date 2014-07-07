1574466092f006d3cad20c25c760a1a2659c3b99
Mats Trovik <hello@matsomatic.co.uk>
2014-07-07 14:57:52 +0100

    https://www.pivotaltracker.com/story/show/74382294 Reworked buttons to draw images programmatically in order to make it easier to change colours if needed.

--- Transfer/Views/GreenButton.m
+++ Transfer/Views/GreenButton.m
@@ -29,33 +29,124 @@
 
 -(void)commonSetup
 {
-
     self.compoundStyle = @"greenButton";
+    self.exclusiveTouch = YES;
     
-    if(IPAD)
+    if(self.addShadow)
     {
-        [self setBackgroundImage:[UIImage imageNamed:@"GreenButton"] forState:UIControlStateNormal];
-        [self setBackgroundImage:[UIImage imageNamed:@"GreenButtonSelected"] forState:UIControlStateHighlighted];
+        [self setBackgroundImage:[self normalImage] forState:UIControlStateNormal];
+        [self setBackgroundImage:[self selectedimage] forState:UIControlStateHighlighted];
+        
+    }
+}
+
+-(void)setAddShadow:(BOOL)addShadow
+{
+    _addShadow = addShadow;
+    
+    if(self.addShadow)
+    {
+        // set background images
+        [self setBackgroundImage:[self normalImage] forState:UIControlStateNormal];
+        [self setBackgroundImage:[self selectedimage] forState:UIControlStateHighlighted];
+        
+    }
+    else
+    {
+        //reapply style
+         self.compoundStyle = @"greenButton";
     }
     
-    self.exclusiveTouch = YES;
 }
 
 -(void)setHighlighted:(BOOL)highlighted
 {
     [super setHighlighted:highlighted];
+    if(self.addShadow)
+    {
+        if (highlighted)
+        {
+            self.contentEdgeInsets = UIEdgeInsetsMake(4, 0, 0, 0);
+        }
+        else
+        {
+            self.contentEdgeInsets = UIEdgeInsetsZero;
+        }
+    }
     
-     if(IPAD)
-     {
-         if(highlighted)
-         {
-             self.contentEdgeInsets = UIEdgeInsetsMake(2, 0, 0, 0);
-         }
-         else
-         {
-        self.contentEdgeInsets = UIEdgeInsetsZero;
-         }
-     }
 }
+
+static __weak UIImage* normalImage;
+-(UIImage*)normalImage
+{
+    UIImage *result = normalImage;
+    if (!result)
+    {
+        
+        CGFloat scale = [[UIScreen mainScreen] scale];
+        CGSize size = CGSizeMake(3.0f*scale, 8.0f*scale);
+        
+        CGRect mainRect = CGRectMake(0.0f, 0.0f, 3.0f*scale, 4.0f*scale);
+        
+        
+        CGRect shadowRect = CGRectMake(0.0f, 4.0f * scale, 3.0f*scale, 4.0f*scale);
+        
+        
+        UIColor* greenColor = [UIColor colorFromStyle:@"green"];
+        UIColor* greenShadow = [UIColor colorFromStyle:@"greenShadow"];
+        
+        UIGraphicsBeginImageContextWithOptions(size,YES, 0.0);
+        CGContextRef context = UIGraphicsGetCurrentContext();
+        CGContextSetFillColorWithColor(context, [greenColor CGColor]);
+        CGContextFillRect(context, mainRect);
+        CGContextSetFillColorWithColor(context, [greenShadow CGColor]);
+        CGContextFillRect(context, shadowRect);
+        
+        UIImage* normal = UIGraphicsGetImageFromCurrentImageContext();
+        normal = [normal resizableImageWithCapInsets:UIEdgeInsetsMake(3, 1, 4, 1)];
+        UIGraphicsEndImageContext();
+        result = normal;
+        normalImage = normal;
+    }
+    
+    return result;
+}
+
+static __weak UIImage* selectedImage;
+-(UIImage*)selectedimage
+{
+    UIImage *result = selectedImage;
+    if (!result)
+    {
+        
+        CGFloat scale = [[UIScreen mainScreen] scale];
+        CGSize size = CGSizeMake(3.0f*scale, 8.0f*scale);
+        
+        CGRect mainRect = CGRectMake(0.0f, 2.0f*scale, 3.0f*scale, 2.0f*scale);
+        
+        
+        CGRect shadowRect = CGRectMake(0.0f, 4.0f * scale, 3.0f*scale, 4.0f*scale);
+        
+        
+        UIColor* greenColor = [UIColor colorFromStyle:@"greenSelected"];
+        UIColor* greenShadow = [UIColor colorFromStyle:@"greenShadow"];
+        
+        UIGraphicsBeginImageContextWithOptions(size,NO, 0.0);
+        CGContextRef context = UIGraphicsGetCurrentContext();
+        CGContextSetFillColorWithColor(context, [greenColor CGColor]);
+        CGContextFillRect(context, mainRect);
+        CGContextSetFillColorWithColor(context, [greenShadow CGColor]);
+        CGContextFillRect(context, shadowRect);
+        
+        UIImage* normal = UIGraphicsGetImageFromCurrentImageContext();
+        normal = [normal resizableImageWithCapInsets:UIEdgeInsetsMake(3, 1, 4, 1)];
+        UIGraphicsEndImageContext();
+        result = normal;
+        selectedImage = normal;
+    }
+    
+    return result;
+}
+
 
 @end
