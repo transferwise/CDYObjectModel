3651dd4770df00d76a7152943ee9e9bf73ad842b
Juhan Hion <juhan@transferwise.com>
2014-07-09 14:37:23 +0300

    https://www.pivotaltracker.com/story/show/71561084 Added FloatingLabelTextField

---
+++ Transfer/Views/FloatingLabelTextField.h
@@ -0,0 +1,13 @@
+//
+//  FloatingLabelTextField.h
+//  Transfer
+//
+//  Created by Juhan Hion on 09.07.14.
+//  Copyright (c) 2014 Mooncascade OÜ. All rights reserved.
+//
+
+#import "JVFloatLabeledTextField.h"
+
+@interface FloatingLabelTextField : JVFloatLabeledTextField
+
+@end
