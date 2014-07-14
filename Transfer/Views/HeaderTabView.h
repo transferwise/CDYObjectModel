757b58693ca069ed35dc4fc9685d78d5665f8d20
Mats Trovik <hello@matsomatic.co.uk>
2014-07-14 14:58:01 +0100

    https://www.pivotaltracker.com/story/show/71562164 Extract header tab view

---
+++ Transfer/Views/HeaderTabView.h
@@ -0,0 +1,13 @@
+//
+//  HeaderTabView.h
+//  Transfer
+//
+//  Created by Mats Trovik on 14/07/2014.
+//  Copyright (c) 2014 Mooncascade OÜ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HeaderTabView : UIView
+
+@end
---
+++ Transfer/Views/HeaderTabView.m
@@ -0,0 +1,31 @@
+//
+//  HeaderTabView.m
+//  Transfer
+//
+//  Created by Mats Trovik on 14/07/2014.
+//  Copyright (c) 2014 Mooncascade OÜ. All rights reserved.
+//
+
+#import "HeaderTabView.h"
+
+@implementation HeaderTabView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
