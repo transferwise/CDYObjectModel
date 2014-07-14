7996fbab9f8bff48e989001607657b1ba675ca7b
Mats Trovik <hello@matsomatic.co.uk>
2014-07-14 14:58:14 +0100

    https://www.pivotaltracker.com/story/show/71562164 header tab view

--- Transfer/Views/HeaderTabView.h
+++ Transfer/Views/HeaderTabView.h
@@ -8,6 +8,25 @@
 
 #import <UIKit/UIKit.h>
 
+@class HeaderTabView;
+
+@protocol HeaderTabViewDelegate <NSObject>
+
+@optional
+-(void)headerTabView:(HeaderTabView*)tabView tabTappedAtIndex:(NSUInteger)index;
+
+@end
+
 @interface HeaderTabView : UIView
+
+@property (weak, nonatomic) IBOutlet UIView *separatorLine;
+
+-(void)setSelectedIndex:(NSUInteger)index;
+
+-(void)setTabTitles:(NSArray*)titles;
+
+
+@property (weak,nonatomic) IBOutlet id<HeaderTabViewDelegate> delegate;
+
 
 @end
--- Transfer/Views/HeaderTabView.m
+++ Transfer/Views/HeaderTabView.m
@@ -7,6 +7,16 @@
 //
 
 #import "HeaderTabView.h"
+#import "Constants.h"
+#import "MOMStyle.h"
+
+@interface HeaderTabView ()
+
+@property (nonatomic, weak) UIButton * lastSelectedButton;
+@property (strong, nonatomic) NSMutableArray *presentedButtons;
+
+
+@end
 
 @implementation HeaderTabView
 
@@ -19,13 +29,77 @@
     return self;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
+-(void)layoutSubviews
 {
-    // Drawing code
+    [super layoutSubviews];
+    CGRect newFrame = self.separatorLine.frame;
+    newFrame.size.height = 1.0f/[[UIScreen mainScreen] scale];
+    newFrame.origin.y += (1.0f - newFrame.size.height);
+    self.separatorLine.frame = newFrame;
 }
-*/
+
+-(void)setTabTitles:(NSArray*)titles
+{
+    
+    for (UIView *presented in self.presentedButtons) {
+        [presented removeFromSuperview];
+    }
+    
+    for (UIView *view in self.subviews) {
+        //Removing the separation lines
+        if(CGRectGetWidth(view.frame) == 1)
+        [view removeFromSuperview];
+    }
+    
+    self.presentedButtons = [NSMutableArray array];
+    
+    CGFloat groupedCellWidth = CGRectGetWidth(self.frame) - 20;
+    CGFloat gap = IPAD?10.0f:4.0f;
+    NSUInteger index = 0;
+    
+    for (NSString* title in titles) {
+    UIButton *button = [self createButton];
+    [button setTitle:title forState:UIControlStateNormal];
+    CGRect frame = CGRectMake(groupedCellWidth / titles.count * index + 10.0 + gap/2 , 0, (groupedCellWidth / titles.count) - gap, CGRectGetHeight(self.frame));
+    [button setFrame:frame];
+    
+    [self addSubview:button];
+    [self.presentedButtons addObject:button];
+    index ++;
+
+    }
+}
+
+- (UIButton *)createButton {
+    UIButton *button = [[UIButton alloc] initWithFrame:CGRectZero];
+    [button setBackgroundImage:[UIImage imageNamed:@"DeselectedTab"] forState:UIControlStateNormal];
+    [button setBackgroundImage:[UIImage imageNamed:@"SelectedTab"] forState:UIControlStateSelected];
+    [button setBackgroundImage:[UIImage imageNamed:@"HighlightedTab"] forState:UIControlStateHighlighted];
+    button.fontStyle = @"medium.@17.CoreFont";
+    button.selectedFontStyle = @"medium.@17.TWElectricBlue";
+    [button addTarget:self action:@selector(tabTapped:) forControlEvents:UIControlEventTouchUpInside];
+    return button;
+}
+
+-(void)setSelectedIndex:(NSUInteger)index
+{
+    [self setSelectedButton:self.presentedButtons[index]];
+}
+
+-(void)setSelectedButton:(UIButton*)button
+{
+    self.lastSelectedButton.selected = NO;
+    self.lastSelectedButton = button;
+    self.lastSelectedButton.selected = YES;
+}
+
+- (void)tabTapped:(UIButton*)sender {
+    NSUInteger index = [self.presentedButtons indexOfObject:sender];
+    [self setSelectedButton:sender];
+    if([self.delegate respondsToSelector:@selector(headerTabView:tabTappedAtIndex:)])
+    {
+        [self.delegate headerTabView:self tabTappedAtIndex:index];
+    }
+}
 
 @end
