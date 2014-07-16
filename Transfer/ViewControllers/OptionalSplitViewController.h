a2b7ad8965d8435d21e8c5c7b816fe33a73223ef
Mats Trovik <hello@matsomatic.co.uk>
2014-07-16 12:09:18 +0100

    https://www.pivotaltracker.com/story/show/71563238 documentation

--- Transfer/ViewControllers/OptionalSplitViewController.h
+++ Transfer/ViewControllers/OptionalSplitViewController.h
@@ -8,6 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
+
+/**
+ *  View controller intented to provide the option of a split view on iPad and normal navigation controller stack on iPhone simply by only assigning a detailContainer view in the iPad nib and not on iPhone.
+ */
 @interface OptionalSplitViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIView *detailContainer;
+
+/**
+ *  shows the argument viewcontroller in the detailContainer if it is assigned. Otherwise, pushes on the nav stack.
+ *
+ *  @param controller view controller to show.
+ */
+-(void)presentDetail:(UIViewController*)controller;
 @end
