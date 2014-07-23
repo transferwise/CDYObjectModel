2f100ac022d0f417469140cf1004328e0275e936
Juhan Hion <juhan@transferwise.com>
2014-07-23 12:07:28 +0300

    https://www.pivotaltracker.com/story/show/71563120 Fixed the last separator

---
+++ Transfer/Cells/FixSeparatorCell.h
@@ -0,0 +1,13 @@
+//
+//  FixSeparatorCell.h
+//  Transfer
+//
+//  Created by Juhan Hion on 23.07.14.
+//  Copyright (c) 2014 Mooncascade OÜ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FixSeparatorCell : UITableViewCell
+
+@end
