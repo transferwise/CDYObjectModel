2cfcf5f823d7dcf624d57a0cbd7d26ca81a04b08
Juhan Hion <juhan@transferwise.com>
2014-07-23 13:45:12 +0300

    https://www.pivotaltracker.com/story/show/71561440 Added empty TabbedHeaderViewController.

---
+++ Transfer/ViewControllers/TabbedHeaderViewController.h
@@ -0,0 +1,13 @@
+//
+//  TabbedHeaderViewController.h
+//  Transfer
+//
+//  Created by Juhan Hion on 23.07.14.
+//  Copyright (c) 2014 Mooncascade OÜ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabbedHeaderViewController : UIViewController
+
+@end
