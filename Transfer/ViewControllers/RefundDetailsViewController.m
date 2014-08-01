c8ae8a92bdb622babe71b5e0c59ed15426e8eb78
Mats Trovik <hello@matsomatic.co.uk>
2014-08-01 11:35:09 +0100

    https://www.pivotaltracker.com/story/show/71561650 iPad working

--- Transfer/ViewControllers/RefundDetailsViewController.m
+++ Transfer/ViewControllers/RefundDetailsViewController.m
@@ -34,6 +34,7 @@
 #import "UserRecipientsOperation.h"
 #import "Credentials.h"
 #import "RecipientEntrySelectionCell.h"
+#import "UIResponder+FirstResponder.h"
 
 CGFloat const TransferHeaderPaddingTop = 40;
 CGFloat const TransferHeaderPaddingBottom = 0;
@@ -61,6 +62,10 @@ CGFloat const TransferHeaderPaddingBottom = 0;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *secondColumnTopConstraint;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *secondColumnLeftEdgeConstraint;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *firstColumnLeftMargin;
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *headerLeftEdgeConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *headerRightEdgeConstraint;
+
 
 
 
@@ -145,18 +150,26 @@ CGFloat const TransferHeaderPaddingBottom = 0;
 
 -(void)configureForInterfaceOrientation:(UIInterfaceOrientation)orientation
 {
+    
+    //Lots of magic numbers here to match designs. Not sure what to do...
     if(UIInterfaceOrientationIsPortrait(orientation))
     {
         self.firstColumnLeftMargin.constant = 204.0f;
         
         self.secondColumnLeftEdgeConstraint.constant = -360;
         self.secondColumnTopConstraint.constant = self.firstColumnHeightConstraint.constant + 60.0f;
+        self.headerLeftEdgeConstraint.constant = 104.0f;
+        self.headerRightEdgeConstraint.constant = -104.0f;
+        
     }
     else
     {
         self.firstColumnLeftMargin.constant = 100.0f;
         self.secondColumnLeftEdgeConstraint.constant = 100.0f;
-        self.secondColumnTopConstraint.constant = 0.0f;
+        self.secondColumnTopConstraint.constant = 17.0f;
+        
+        self.headerLeftEdgeConstraint.constant = 0.0f;
+        self.headerRightEdgeConstraint.constant = 0.0f;
     }
 }
 
@@ -175,6 +188,12 @@ CGFloat const TransferHeaderPaddingBottom = 0;
         return;
     }
 
+    [self.navigationController setNavigationBarHidden:NO animated:YES];
+    if(! self.navigationItem.leftBarButtonItem)
+    {
+        [self.navigationItem setLeftBarButtonItem:[TransferBackButtonItem backButtonForPoppedNavigationController:self.navigationController]];
+    }
+    
     [[AnalyticsCoordinator sharedInstance] refundDetailsScreenShown];
 
     TRWProgressHUD *hud = [TRWProgressHUD showHUDOnView:self.navigationController.view];
@@ -456,5 +475,111 @@ CGFloat const TransferHeaderPaddingBottom = 0;
         }
     });
 }
+
+#pragma mark - Keyboard show/hide
+
+
+
+
+-(void)keyboardWillShow:(NSNotification*)note
+{
+    CGRect newframe = [self.view convertRect:[note.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue] fromView:self.view.window];
+    if(IPAD)
+    {
+        NSTimeInterval duration = [note.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+        UIViewAnimationCurve curve = [note.userInfo[UIKeyboardAnimationCurveUserInfoKey] integerValue];
+        
+        CGFloat overlap = self.containerScrollView.frame.origin.y + self.containerScrollView.frame.size.height - newframe.origin.y;
+        
+        if(overlap >0)
+        {
+            
+            [UIView beginAnimations:nil context:NULL];
+            [UIView setAnimationDuration:duration];
+            [UIView setAnimationCurve:curve];
+            [UIView setAnimationBeginsFromCurrentState:YES];
+            
+            if(UIEdgeInsetsEqualToEdgeInsets(self.cachedInsets, UIEdgeInsetsZero))
+            {
+                self.cachedInsets = self.containerScrollView.contentInset;
+            }
+            
+            UIEdgeInsets newInsets = self.cachedInsets;
+            newInsets.bottom += overlap;
+            self.containerScrollView.contentInset = newInsets;
+            
+            UIView *firstResponder = [UIResponder currentFirstResponder];
+            if(firstResponder)
+            {
+                [self scrollScrollViewToShowView:firstResponder];
+            }
+            
+            
+            [UIView commitAnimations];
+            
+            
+        }
+    }
+    else
+    {
+        [super keyboardWillShow:note];
+    }
+}
+
+-(void)keyboardWillHide:(NSNotification*)note
+{
+    if(IPAD)
+    {
+        NSTimeInterval duration = [note.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+        UIViewAnimationCurve curve = [note.userInfo[UIKeyboardAnimationCurveUserInfoKey] integerValue];
+        
+        
+        [UIView beginAnimations:nil context:NULL];
+        [UIView setAnimationDuration:duration];
+        [UIView setAnimationCurve:curve];
+        [UIView setAnimationBeginsFromCurrentState:YES];
+        
+        self.containerScrollView.contentInset = self.cachedInsets;
+        
+        [UIView commitAnimations];
+        
+        self.cachedInsets = UIEdgeInsetsZero;
+    }
+    else
+    {
+        [super keyboardWillHide:note];
+    }
+}
+
+-(void)textFieldEntryFinished
+{
+    if (!IPAD)
+    {
+        ((UITableView*)self.tableViews[0]).scrollEnabled = YES;
+    }
+}
+
+-(void)scrollToCell:(UITableViewCell *)cell inTableView:(UITableView *)tableView
+{
+    if(IPAD)
+    {
+        [self scrollScrollViewToShowView:cell];
+    }
+    else
+    {
+        [super scrollToCell:cell inTableView:tableView];
+        tableView.scrollEnabled = NO;
+    }
+}
+
+-(void)scrollScrollViewToShowView:(UIView*)targetView
+{
+    
+    CGRect showRect = CGRectMake(0, self.containerScrollView.contentSize.height - 1, 1, 1);
+    [self.containerScrollView scrollRectToVisible:showRect animated:NO];
+    
+}
+
+
 
 @end
