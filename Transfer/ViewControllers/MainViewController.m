bb1a815149842634e45a77452cba34bd10db31ae
Mats Trovik <hello@matsomatic.co.uk>
2014-08-05 15:41:18 +0100

    https://www.pivotaltracker.com/story/show/71560024 Review feedback iPhone

--- Transfer/ViewControllers/MainViewController.m
+++ Transfer/ViewControllers/MainViewController.m
@@ -176,7 +176,7 @@
 
 - (void)presentIntroductionController:(BOOL)shownBefore {
     UIViewController *presented;
-    if (NO){//shownBefore || [self.objectModel hasIntroBeenShown]) {
+    if (shownBefore || [self.objectModel hasIntroBeenShown]) {
         if ([self.objectModel shouldShowDirectUserSignup]) {
             SignUpViewController *controller = [[SignUpViewController alloc] init];
             [controller setObjectModel:self.objectModel];
