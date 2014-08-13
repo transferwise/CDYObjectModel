b4ea771e98070bb0362603638898008817e61b16
Mats Trovik <hello@matsomatic.co.uk>
2014-08-13 10:05:32 +0100

    https://www.pivotaltracker.com/story/show/71564116 starting work on invitations

--- Transfer/ViewControllers/InvitationsViewController.m
+++ Transfer/ViewControllers/InvitationsViewController.m
@@ -9,6 +9,14 @@
 #import "InvitationsViewController.h"
 
 @interface InvitationsViewController ()
+@property (weak, nonatomic) IBOutlet UIView *profilePictureContainer;
+
+@property (strong, nonatomic) IBOutletCollection(UIImageView) NSArray *profilePictures;
+@property (weak, nonatomic) IBOutlet UIButton *inviteButton;
+@property (weak, nonatomic) IBOutlet UILabel *headerLabel;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *buttonTopSpaceConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *profileImagesContainerHeightConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *profileImagesOntainerTopSpaceConstraint;
 
 @end
 
@@ -26,13 +34,23 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    self.headerLabel.text = NSLocalizedString(@"invite.header", nil);
+    [self.inviteButton setTitle:NSLocalizedString(@"invite.button.title", nil) forState:UIControlStateNormal];
+}
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    self.title = NSLocalizedString(@"invite.controller.title", nil);
+    [self.navigationController setNavigationBarHidden:NO animated:YES];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)inviteButtonTapped:(id)sender {
 }
 
 @end
