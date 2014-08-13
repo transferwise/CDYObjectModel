0537d28432efdb1be594ce8fb56ae3b1384f76f7
Mats Trovik <hello@matsomatic.co.uk>
2014-08-13 10:05:03 +0100

    https://www.pivotaltracker.com/story/show/71564116 starting work on invitation screen

---
+++ Transfer/ViewControllers/InvitationsViewController.h
@@ -0,0 +1,13 @@
+//
+//  InvitationsViewController.h
+//  Transfer
+//
+//  Created by Mats Trovik on 13/08/2014.
+//  Copyright (c) 2014 Mooncascade OÜ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InvitationsViewController : UIViewController
+
+@end
---
+++ Transfer/ViewControllers/InvitationsViewController.m
@@ -0,0 +1,38 @@
+//
+//  InvitationsViewController.m
+//  Transfer
+//
+//  Created by Mats Trovik on 13/08/2014.
+//  Copyright (c) 2014 Mooncascade OÜ. All rights reserved.
+//
+
+#import "InvitationsViewController.h"
+
+@interface InvitationsViewController ()
+
+@end
+
+@implementation InvitationsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
