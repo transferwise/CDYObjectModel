e5b1b94fe4f36c1f69e658f5c0dd9f23a641657a
Mats Trovik <mats@matsomatic.co.uk>
2014-08-18 15:30:51 +0100

    https://www.pivotaltracker.com/story/show/77116284 review feedback

---
+++ Transfer/Network/PaymentcancelOperation.h
@@ -0,0 +1,13 @@
+//
+//  PaymentcancelOperation.h
+//  Transfer
+//
+//  Created by Mats Trovik on 18/08/2014.
+//  Copyright (c) 2014 Mooncascade OÜ. All rights reserved.
+//
+
+#import "TransferwiseOperation.h"
+
+@interface PaymentcancelOperation : TransferwiseOperation
+
+@end
---
+++ Transfer/Network/PaymentcancelOperation.m
@@ -0,0 +1,13 @@
+//
+//  PaymentcancelOperation.m
+//  Transfer
+//
+//  Created by Mats Trovik on 18/08/2014.
+//  Copyright (c) 2014 Mooncascade OÜ. All rights reserved.
+//
+
+#import "PaymentcancelOperation.h"
+
+@implementation PaymentcancelOperation
+
+@end
