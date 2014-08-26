55e1434d42f631eecf328a564919c8eb7f97c808
Mats Trovik <mats@matsomatic.co.uk>
2014-08-26 12:52:32 +0100

    https://www.pivotaltracker.com/story/show/77559192 Start work on state suggestion

---
+++ Transfer/Utils/StateSuggestionProvider.h
@@ -0,0 +1,13 @@
+//
+//  StateSuggestionProvider.h
+//  Transfer
+//
+//  Created by Mats Trovik on 26/08/2014.
+//  Copyright (c) 2014 Mooncascade OÜ. All rights reserved.
+//
+
+#import "SuggestionCellProvider.h"
+
+@interface StateSuggestionProvider : SuggestionCellProvider
+
+@end
---
+++ Transfer/Utils/StateSuggestionProvider.m
@@ -0,0 +1,13 @@
+//
+//  StateSuggestionProvider.m
+//  Transfer
+//
+//  Created by Mats Trovik on 26/08/2014.
+//  Copyright (c) 2014 Mooncascade OÜ. All rights reserved.
+//
+
+#import "StateSuggestionProvider.h"
+
+@implementation StateSuggestionProvider
+
+@end
