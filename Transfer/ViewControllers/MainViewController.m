b219affe4ce1e0a4b1e74183c376d1b5faf53569
Juhan Hion <juhan@transferwise.com>
2014-09-11 15:02:02 +0300

    https://www.pivotaltracker.com/story/show/77781250 all the pieces put back together.

--- Transfer/ViewControllers/MainViewController.m
+++ Transfer/ViewControllers/MainViewController.m
@@ -35,9 +35,11 @@
 
 @implementation MainViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
+    if (self)
+	{
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(loggedOut) name:TRWLoggedOutNotification object:nil];
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(moveToPaymentsList) name:TRWMoveToPaymentsListNotification object:nil];
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(moveToPaymentView) name:TRWMoveToPaymentViewNotification object:nil];
@@ -45,151 +47,145 @@
     return self;
 }
 
-- (void)dealloc {
+- (void)dealloc
+{
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     
     self.navigationBar.translucent = NO;
+	self.navigationBarHidden = YES;
     
-    TransactionsViewController *transactionsController = [[TransactionsViewController alloc] init];
-    [self setTransactionsController:transactionsController];
-    [transactionsController setObjectModel:self.objectModel];
-    TabItem *transactionsItem = [TabItem new];
-    transactionsItem.viewController = transactionsController;
-    transactionsItem.title = NSLocalizedString(@"transactions.controller.tabbar.title", nil);
-    transactionsItem.icon = [UIImage imageNamed:@"Transfers"];
-    transactionsItem.selectedIcon = [UIImage imageNamed:@"Transfers_selected"];
+	[self setUpTabs];
+    [self setDelegate:self];
+}
 
-    
-    InvitationsViewController* invitationController = [[InvitationsViewController alloc] init];
-    TabItem *inviteItem = [TabItem new];
-    invitationController.objectModel = self.objectModel;
-    inviteItem.title = NSLocalizedString(@"invite.controller.tabbar.title", nil);
-    inviteItem.icon = [UIImage imageNamed:@"Invite"];
-    inviteItem.selectedIcon = [UIImage imageNamed:@"Invite_selected"];
-    inviteItem.viewController = invitationController;
-
-    
-    TabItem *paymentItem = [TabItem new];
-    paymentItem.title = NSLocalizedString(@"payment.controller.tabbar.title", nil);
-    paymentItem.icon = [UIImage imageNamed:@"Payment"];
-    paymentItem.selectedIcon = [UIImage imageNamed:@"Payment_selected"];
-    paymentItem.deSelectedColor = [UIColor colorFromStyle:@"TWBlueHighlighted"];
-    paymentItem.textColor = [UIColor whiteColor];
-    [paymentItem setActionBlock:^(TabItem* item){
-        NewPaymentViewController *controller = [[NewPaymentViewController alloc] init];
-        [controller setObjectModel:self.objectModel];
-       ConnectionAwareViewController *wrapper = [ConnectionAwareViewController createWrappedNavigationControllerWithRoot:controller navBarHidden:YES];
-        [self presentViewController:wrapper animated:YES completion:nil];
-        [[GoogleAnalytics sharedInstance] sendScreen:@"New payment"];
-        return NO;
-    }];
-
-
-    ContactsViewController *contactsController = [[ContactsViewController alloc] init];
-    [contactsController setObjectModel:self.objectModel];
-    TabItem *contactsItem = [TabItem new];
-    contactsItem.title = NSLocalizedString(@"contacts.controller.tabbar.title", nil);
-    contactsItem.icon = [UIImage imageNamed:@"Contacts"];
-    contactsItem.selectedIcon = [UIImage imageNamed:@"Contacts_selected"];
-    contactsItem.viewController = contactsController;
-    
+- (void)setUpTabs
+{
+	TransactionsViewController *transactionsController = [[TransactionsViewController alloc] init];
+	[self setTransactionsController:transactionsController];
+	[transactionsController setObjectModel:self.objectModel];
+	TabItem *transactionsItem = [TabItem new];
+	transactionsItem.viewController = transactionsController;
+	transactionsItem.title = NSLocalizedString(@"transactions.controller.tabbar.title", nil);
+	transactionsItem.icon = [UIImage imageNamed:@"Transfers"];
+	transactionsItem.selectedIcon = [UIImage imageNamed:@"Transfers_selected"];
+	
+	
+	InvitationsViewController* invitationController = [[InvitationsViewController alloc] init];
+	TabItem *inviteItem = [TabItem new];
+	invitationController.objectModel = self.objectModel;
+	inviteItem.title = NSLocalizedString(@"invite.controller.tabbar.title", nil);
+	inviteItem.icon = [UIImage imageNamed:@"Invite"];
+	inviteItem.selectedIcon = [UIImage imageNamed:@"Invite_selected"];
+	inviteItem.viewController = invitationController;
+	
+	
+	TabItem *paymentItem = [TabItem new];
+	paymentItem.title = NSLocalizedString(@"payment.controller.tabbar.title", nil);
+	paymentItem.icon = [UIImage imageNamed:@"Payment"];
+	paymentItem.selectedIcon = [UIImage imageNamed:@"Payment_selected"];
+	paymentItem.deSelectedColor = [UIColor colorFromStyle:@"TWBlueHighlighted"];
+	paymentItem.textColor = [UIColor whiteColor];
+	[paymentItem setActionBlock:^(TabItem* item){
+		NewPaymentViewController *controller = [[NewPaymentViewController alloc] init];
+		[controller setObjectModel:self.objectModel];
+		ConnectionAwareViewController *wrapper = [ConnectionAwareViewController createWrappedNavigationControllerWithRoot:controller navBarHidden:YES];
+		[self presentViewController:wrapper animated:YES completion:nil];
+		[[GoogleAnalytics sharedInstance] sendScreen:@"New payment"];
+		return NO;
+	}];
+	
+	
+	ContactsViewController *contactsController = [[ContactsViewController alloc] init];
+	[contactsController setObjectModel:self.objectModel];
+	TabItem *contactsItem = [TabItem new];
+	contactsItem.title = NSLocalizedString(@"contacts.controller.tabbar.title", nil);
+	contactsItem.icon = [UIImage imageNamed:@"Contacts"];
+	contactsItem.selectedIcon = [UIImage imageNamed:@"Contacts_selected"];
+	contactsItem.viewController = contactsController;
+	
 	ProfilesEditViewController *profileController = [[ProfilesEditViewController alloc] init];
 	[profileController setObjectModel:self.objectModel];
 	
 	TabItem *profileItem = [TabItem new];
-    profileItem.title = NSLocalizedString(@"profile.controller.tabbar.title", nil);
-    profileItem.icon = [UIImage imageNamed:@"Profile"];
-    profileItem.selectedIcon = [UIImage imageNamed:@"Profile_selected"];
+	profileItem.title = NSLocalizedString(@"profile.controller.tabbar.title", nil);
+	profileItem.icon = [UIImage imageNamed:@"Profile"];
+	profileItem.selectedIcon = [UIImage imageNamed:@"Profile_selected"];
 	profileItem.viewController = IPAD ? [[UINavigationController alloc] initWithRootViewController:profileController] : profileController;
-    TabViewController *tabController = [[TabViewController alloc] init];
-    tabController.defaultSelectedColor = [UIColor colorFromStyle:@"TWBlue"];
-    tabController.defaultDeSelectedColor = [UIColor colorFromStyle:@"TWBlue"];
-    tabController.defaultHighlightedColor = [UIColor colorFromStyle:@"TWBlueHighlighted"];
-    
-    if(IPAD)
-    {
-        [tabController setItems:@[paymentItem,transactionsItem,inviteItem,contactsItem,profileItem]];
-        tabController.tabBarInsets = UIEdgeInsetsMake(20, 0, 20, 0);
+	TabViewController *tabController = [[TabViewController alloc] init];
+	tabController.defaultSelectedColor = [UIColor colorFromStyle:@"TWBlue"];
+	tabController.defaultDeSelectedColor = [UIColor colorFromStyle:@"TWBlue"];
+	tabController.defaultHighlightedColor = [UIColor colorFromStyle:@"TWBlueHighlighted"];
+	
+	if(IPAD)
+	{
+		[tabController setItems:@[paymentItem,transactionsItem,inviteItem,contactsItem,profileItem]];
+		tabController.tabBarInsets = UIEdgeInsetsMake(20, 0, 20, 0);
 		tabController.centerVertically = YES;
-        [self setNavigationBarHidden:YES];
-    }
-    else
-    {
-        [tabController setItems:@[transactionsItem,inviteItem,paymentItem,contactsItem,profileItem]];
-    }
-    
-    [tabController selectItem:transactionsItem];
-    
-    self.tabController = tabController;
-
-    [self setViewControllers:@[tabController]];
-
-    [self setDelegate:self];
+		[self setNavigationBarHidden:YES];
+	}
+	else
+	{
+		[tabController setItems:@[transactionsItem,inviteItem,paymentItem,contactsItem,profileItem]];
+	}
+	
+	[tabController selectItem:transactionsItem];
+	self.tabController = tabController;
+	
+	if (![Credentials userLoggedIn])
+	{
+		[self setViewControllers:@[[self getAnonumousViewController]]];
+	}
+	else
+	{
+		[self setViewControllers:@[self.tabController]];
+	}
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-- (void)viewWillAppear:(BOOL)animated {
+- (void)viewWillAppear:(BOOL)animated
+{
     [super viewWillAppear:animated];
 
-    if (self.launchTableViewGamAdjustmentDone) {
+    if (self.launchTableViewGamAdjustmentDone)
+	{
         return;
     }
 
     [self setLaunchTableViewGamAdjustmentDone:YES];
-	
-    if (![Credentials userLoggedIn])
-	{
-        NewPaymentViewController *controller = [[NewPaymentViewController alloc] init];
-        [controller setObjectModel:self.objectModel];
-        [controller setDummyPresentation:YES];
-        [self setNavigationBarHidden:YES];
-        [self pushViewController:controller animated:NO];
-    }
-}
-
-- (void)viewDidAppear:(BOOL)animated {
-    [super viewDidAppear:animated];
-
-	[self presentNewPaymentViewController];
-	[self setShown:YES];
 }
 
 - (void)loggedOut
 {
 	[self clearData];
-    [self presentNewPaymentViewController];
+	
+	[self presentViewController:[self getAnonumousViewController]
+					   animated:YES
+					 completion:nil];
+	[self popToRootViewControllerAnimated:NO];
 }
 
-- (void)presentNewPaymentViewController
+- (ConnectionAwareViewController *)getAnonumousViewController
 {
-	if (![Credentials userLoggedIn])
+	UIViewController *presented;
+	if ([self.objectModel shouldShowDirectUserSignup])
 	{
-		UIViewController *presented;
-		if ([self.objectModel shouldShowDirectUserSignup])
-		{
-			SignUpViewController *controller = [[SignUpViewController alloc] init];
-			[controller setObjectModel:self.objectModel];
-			presented = controller;
-		}
-		else
-		{
-			NewPaymentViewController *controller = [[NewPaymentViewController alloc] init];
-			[controller setObjectModel:self.objectModel];
-			presented = controller;
-		}
-		
-		ConnectionAwareViewController *wrapper = [ConnectionAwareViewController createWrappedNavigationControllerWithRoot:presented navBarHidden:YES];
-		[self presentViewController:wrapper animated:YES completion:nil];
-		[self popToRootViewControllerAnimated:NO];
+		SignUpViewController *controller = [[SignUpViewController alloc] init];
+		[controller setObjectModel:self.objectModel];
+		presented = controller;
 	}
+	else
+	{
+		NewPaymentViewController *controller = [[NewPaymentViewController alloc] init];
+		[controller setObjectModel:self.objectModel];
+		presented = controller;
+	}
+	
+	ConnectionAwareViewController *wrapper = [ConnectionAwareViewController createWrappedNavigationControllerWithRoot:presented navBarHidden:YES];
+	return wrapper;
 }
 
 - (void)clearData
@@ -198,11 +194,13 @@
 	[self.transactionsController clearData];
 }
 
-- (void)navigationController:(UINavigationController *)navigationController didShowViewController:(UIViewController *)viewController animated:(BOOL)animated {
+- (void)navigationController:(UINavigationController *)navigationController didShowViewController:(UIViewController *)viewController animated:(BOOL)animated
+{
     NSUInteger numberOfControllers = [navigationController.viewControllers count];
     
     NSArray *recognizers = [navigationController.navigationBar gestureRecognizers];
-    for (UIGestureRecognizer *recognizer in recognizers) {
+    for (UIGestureRecognizer *recognizer in recognizers)
+	{
         [recognizer setEnabled:numberOfControllers == 1];
     }
 }
@@ -212,15 +210,19 @@
     
 }
 
-- (void)moveToPaymentsList {
+- (void)moveToPaymentsList
+{
     [self.tabController selectIndex:IPAD?1:0];
     [self popToRootViewControllerAnimated:YES];
+	[self setViewControllers:@[self.tabController]];
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
-- (void)moveToPaymentView {
+- (void)moveToPaymentView
+{
     [self.tabController selectIndex:IPAD?0:2];
     [self popToRootViewControllerAnimated:YES];
+	[self setViewControllers:@[self.tabController]];
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
