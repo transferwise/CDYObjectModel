d13bd255a22facb3121aad18533b1ca8d675d9c0
Juhan Hion <juhan@transferwise.com>
2014-09-24 16:40:46 +0300

    https://www.pivotaltracker.com/story/show/78356968 Replaced Contact Support button on completed transfers with Rate the app button.

--- Transfer/ViewControllers/TransferDetailsViewController.m
+++ Transfer/ViewControllers/TransferDetailsViewController.m
@@ -102,7 +102,14 @@
     }
     self.statusIcon.image = icon;
 	
-	[self.supportButton setTitle:NSLocalizedString([@"transferdetails.controller.button.support" deviceSpecificLocalization], nil) forState:UIControlStateNormal];
+	if (self.payment.status == PaymentStatusTransferred)
+	{
+		[self.supportButton setTitle:NSLocalizedString([@"transferdetails.controller.button.rate" deviceSpecificLocalization], nil) forState:UIControlStateNormal];
+	}
+	else
+	{
+		[self.supportButton setTitle:NSLocalizedString([@"transferdetails.controller.button.support" deviceSpecificLocalization], nil) forState:UIControlStateNormal];
+	}
 	
 	[self.view layoutIfNeeded];
 }
@@ -169,9 +176,16 @@
 
 - (IBAction)contactSupportPressed
 {
-    [[GoogleAnalytics sharedInstance] sendAppEvent:@"ContactSupport" withLabel:@"Transfer details"];
-    NSString *subject = [NSString stringWithFormat:NSLocalizedString(@"support.email.payment.subject.base", nil), self.payment.remoteId];
-    [[SupportCoordinator sharedInstance] presentOnController:self emailSubject:subject];
+	if (self.payment.status == PaymentStatusTransferred)
+	{
+		[[UIApplication sharedApplication] openURL:[NSURL URLWithString:TRWRateAppUrl]];
+	}
+	else
+	{
+		[[GoogleAnalytics sharedInstance] sendAppEvent:@"ContactSupport" withLabel:@"Transfer details"];
+		NSString *subject = [NSString stringWithFormat:NSLocalizedString(@"support.email.payment.subject.base", nil), self.payment.remoteId];
+		[[SupportCoordinator sharedInstance] presentOnController:self emailSubject:subject];
+	}
 }
 
 - (NSString*)getStatusBasedLocalization:(NSString *)localizationKey status:(NSString*)status
