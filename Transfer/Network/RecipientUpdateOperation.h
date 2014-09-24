29fe2ec50b934a9798f0fece26527f67c5ab6c15
Mats Trovik <mats@matsomatic.co.uk>
2014-09-24 14:15:30 +0100

    https://www.pivotaltracker.com/story/show/77621164 update operation

--- Transfer/Network/RecipientUpdateOperation.h
+++ Transfer/Network/RecipientUpdateOperation.h
@@ -7,7 +7,15 @@
 //
 
 #import "TransferwiseOperation.h"
+@class Recipient;
+@class ObjectModel;
 
 @interface RecipientUpdateOperation : TransferwiseOperation
+
+@property (nonatomic, strong) Recipient* recipient;
+@property (nonatomic, strong) ObjectModel* objectModel;
+@property (nonatomic, copy) void(^completionHandler)(NSError* error);
+
++(instancetype)instanceWithRecipient:(Recipient*)recipient objectModel:(ObjectModel*)objectModel completionHandler:(void(^)(NSError* error))completionHandler;
 
 @end
