71d38510047671b383d721595d69af2e67a8324c
Mats Trovik <mats@matsomatic.co.uk>
2014-10-10 11:30:38 +0100

    https://www.pivotaltracker.com/story/show/80228494 Upfront password reset

--- Transfer/ViewControllers/LoginViewController.m
+++ Transfer/ViewControllers/LoginViewController.m
@@ -28,6 +28,8 @@
 #import "MainViewController.h"
 #import "ConnectionAwareViewController.h"
 
+IB_DESIGNABLE
+
 @interface LoginViewController () <UITextFieldDelegate>
 
 @property (strong, nonatomic) IBOutlet FloatingLabelTextField *emailTextField;
@@ -41,6 +43,8 @@
 @property (strong, nonatomic) IBOutlet NSLayoutConstraint *passwordSeparatorHeight;
 @property (strong, nonatomic) LoginHelper *loginHelper;
 @property (weak, nonatomic) IBOutlet UIButton *touchIdButton;
+
+@property (nonatomic, strong) IBInspectable NSString* xibNameForResetPassword;
 
 @end
 
@@ -210,7 +214,7 @@
 #pragma mark - Password reset
 - (void)forgotPasswordTapped
 {
-    ResetPasswordViewController *controller = [[ResetPasswordViewController alloc] init];
+    ResetPasswordViewController *controller = self.xibNameForResetPassword?[[ResetPasswordViewController alloc] initWithNibName:self.xibNameForResetPassword bundle:nil]:[[ResetPasswordViewController alloc] init];
     [controller setObjectModel:self.objectModel];
 	controller.delegate = self;
     [self.navigationController pushViewController:controller animated:YES];
