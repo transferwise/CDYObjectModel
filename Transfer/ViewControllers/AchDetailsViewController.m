b0e0dbfade6248d3fbde44aa2b44f64918be907e
juhanh <juhan.hion@gmail.com>
2014-11-18 14:16:56 +0200

    More incremental changes [#82468546]

--- Transfer/ViewControllers/AchDetailsViewController.m
+++ Transfer/ViewControllers/AchDetailsViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "AchDetailsViewController.h"
+#import "NSString+DeviceSpecificLocalisation.h"
 
 @interface AchDetailsViewController ()
 
@@ -16,24 +17,11 @@
 
 @implementation AchDetailsViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+	[self setTitle:NSLocalizedString(@"", nil)];
+	[self.supportButton setTitle:NSLocalizedString([@"ach.controller.button.support" deviceSpecificLocalization], nil) forState:UIControlStateNormal];
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
