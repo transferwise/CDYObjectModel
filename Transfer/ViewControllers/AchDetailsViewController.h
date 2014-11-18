1c91bf4515bfa6db3beaeaadc18f2836d0a268e0
juhanh <juhan.hion@gmail.com>
2014-11-18 13:36:12 +0200

    Started work with ACH account and routing number entering view [#82468546]

---
+++ Transfer/ViewControllers/AchDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  AchDetailsViewController.h
+//  Transfer
+//
+//  Created by Juhan Hion on 18.11.14.
+//  Copyright (c) 2014 Mooncascade OÜ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AchDetailsViewController : UIViewController
+
+@end
---
+++ Transfer/ViewControllers/AchDetailsViewController.m
@@ -0,0 +1,39 @@
+//
+//  AchDetailsViewController.m
+//  Transfer
+//
+//  Created by Juhan Hion on 18.11.14.
+//  Copyright (c) 2014 Mooncascade OÜ. All rights reserved.
+//
+
+#import "AchDetailsViewController.h"
+
+@interface AchDetailsViewController ()
+
+@property (strong, nonatomic) IBOutlet UIButton *supportButton;
+
+@end
+
+@implementation AchDetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
