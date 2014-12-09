7735a64b732e8b29ca477b57757b5c953c350272
Mats Trovik <mats@matsomatic.co.uk>
2014-12-09 11:28:00 +0000

    removing AB test in favour of non crashing app

--- Transfer/Constants.h
+++ Transfer/Constants.h
@@ -89,7 +89,6 @@ static NSUInteger const TransferwiseAppID = 612261027;
 #define USE_FACEBOOK_EVENTS 1
 #define USE_APPSFLYER_EVENTS 1
 
-//#define REGISTRATION_UPFRONT_OVERRIDE YES
 
 typedef NS_OPTIONS(short, IdentificationRequired) {
 	IdentificationNoneRequired = 0,
