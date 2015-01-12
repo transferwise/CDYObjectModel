471bd2c1b141720702acd900d1b2a87e28794efb
Mats Trovik <mats@matsomatic.co.uk>
2015-01-12 16:50:17 +0000

    [#85524242] Registration after loggin out now works as expected.

--- Transfer/Utils/AuthenticationHelper.m
+++ Transfer/Utils/AuthenticationHelper.m
@@ -158,8 +158,7 @@
         NewPaymentViewController *paymentView = [[NewPaymentViewController alloc] init];
         [paymentView setObjectModel:objectModel];
 		
-		//SignUpViewController is not ConnectionAwareViewController wrapped
-        if(controller.presentingViewController && ![controller isKindOfClass:[SignUpViewController class]])
+        if(controller.presentingViewController)
         {
             ConnectionAwareViewController* root = (ConnectionAwareViewController*)[controller.navigationController?:controller parentViewController];
             [root replaceWrappedViewControllerWithController:paymentView withAnimationStyle:ConnectionModalAnimation];
