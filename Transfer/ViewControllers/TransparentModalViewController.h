e630b6af24a76ce30a41a37043ab2b992f666e80
Mats Trovik <mats@matsomatic.co.uk>
2015-01-27 13:34:58 +0000

    [delivers: #81093386] added animations to Transparent Modal Viewcontroller

--- Transfer/ViewControllers/TransparentModalViewController.h
+++ Transfer/ViewControllers/TransparentModalViewController.h
@@ -29,6 +29,21 @@ typedef NS_ENUM(short, TransparentModalPresentationStyle)
 @property (nonatomic, weak) id<TransparentModalViewControllerDelegate> delegate;
 @property (nonatomic, assign) TransparentModalPresentationStyle presentationStyle;
 
+/**
+ *  Array of views to slide in horizontally when the modal is presented
+ */
+@property (nonatomic, strong) IBOutletCollection(UIView) NSArray* slideInAnimationViews;
+/**
+ *  Array of horisontal layout constraints relative to the superview's edge used to slide in views horizontally. 
+ *  The constraints must match up with the slideInAnimationViews array
+ */
+@property (nonatomic, strong) IBOutletCollection(NSLayoutConstraint) NSArray* slideInAnimationConstraints;
+
+/**
+ *  Array of views to fade in when the modal is presented
+ */
+@property (nonatomic, strong) IBOutletCollection(UIView) NSArray* fadeInAnimationViews;
+
 -(void)presentOnViewController:(UIViewController*)hostViewcontroller withPresentationStyle:(TransparentModalPresentationStyle)presentationStyle;
 -(void)presentOnViewController:(UIViewController*)hostViewcontroller;
 -(IBAction)dismiss;
