760344c52e506be69130dcb272eff6706ea5a64d
Mats Trovik <mats@matsomatic.co.uk>
2015-01-30 16:05:02 +0000

    [delivers: #87281422] Long press or double tap to bring up copy menu on presentation cells.

---
+++ Transfer/Views/CopyLabel.h
@@ -0,0 +1,13 @@
+//
+//  CopyLabel.h
+//  Transfer
+//
+//  Created by Mats Trovik on 30/01/2015.
+//  Copyright (c) 2015 Mooncascade OÜ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CopyLabel : UILabel
+
+@end
