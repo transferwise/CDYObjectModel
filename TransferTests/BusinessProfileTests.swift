6ed7dfbf14552fdad053cded6079772fb7679180
Nick Banks <nick.banks@synchromation.com>
2015-06-29 18:07:42 +0100

    More tests

--- TransferTests/BusinessProfileTests.swift
+++ TransferTests/BusinessProfileTests.swift
@@ -9,65 +9,55 @@
 import UIKit
 import XCTest
 
+// Set up some default constants for use in testing
+extension BusinessProfile {
+
+    // It would have been more pleasant to create a set of derived Mock classes, but this is not easily possible
+    // with NSManagedObjects
+    func setupAsGBR () {
+        addressFirstLine = "186-188 City Road"
+        businessDescription = "Software Development"
+        city = "London"
+        companyRole = "Company Director"
+        companyType = "Company Limited By Guarantee"
+        countryCode = "GBR"
+        name = "Dave Spart"
+        postCode = "EC1V 2NT"
+        registrationNumber = "07209813"
+    }
+    
+    func setupAsUSA () {
+        addressFirstLine = "1708 Oakland Road, Suite 500"
+        businessDescription = "Software Development"
+        city = "San Jose"
+        companyRole = "Company Director"
+        companyType = "Company Limited By Guarantee"
+        countryCode = "USA"
+        name = "Dave Spart"
+        postCode = "95131"
+        registrationNumber = "07209813"
+        state = "California"
+    }
+    
+    func setupAsAUS () {
+        abn = "121181976"
+        acn = "0123454321"
+        addressFirstLine = " 50 Hunter St,"
+        arbn = ""
+        businessDescription = "Software Development"
+        city = "Sydney"
+        companyRole = "Company Director"
+        companyType = "Company Limited By Guarantee"
+        countryCode = "AUS"
+        name = "Dave Spart"
+        postCode = "2000"
+        registrationNumber = "07209813"
+    }
+}
+
 class BusinessProfileTests: XCTestCase {
     
     let objectModel = ObjectModel();
-    
-    // Set up some default constants for use in testing
-    class MockUSABusinessProfile : BusinessProfile {
-        
-        override func awakeFromInsert() {
-            super.awakeFromInsert()
-            
-            addressFirstLine = "1708 Oakland Road, Suite 500"
-            businessDescription = ""
-            city = "San Jose"
-            companyRole = ""
-            companyType = ""
-            countryCode = "USA"
-            name = "Dave Spart"
-            postCode = "95131"
-            registrationNumber = "07209813"
-            state = "California"
-        }
-    }
-    
-    class MockAUSBusinessProfile : BusinessProfile {
-        
-        override func awakeFromInsert() {
-            super.awakeFromInsert()
-            
-            abn = "121181976"
-            acn = "0123454321"
-            addressFirstLine = " 50 Hunter St,"
-            arbn = ""
-            businessDescription = ""
-            city = "Sydney"
-            companyRole = ""
-            companyType = ""
-            countryCode = "AUS"
-            name = "Dave Spart"
-            postCode = "2000"
-            registrationNumber = "07209813"
-        }
-    }
-    
-    class MockGBRBusinessProfile : BusinessProfile {
-        
-        override func awakeFromInsert() {
-            super.awakeFromInsert()
-            
-            addressFirstLine = "186-188 City Road"
-            businessDescription = "x"
-            city = "London"
-            companyRole = "x"
-            companyType = "x"
-            countryCode = "GBR"
-            name = "Dave Spart"
-            postCode = "EC1V 2NT"
-            registrationNumber = "07209813"
-        }
-    }
 
     override func setUp() {
         super.setUp()
@@ -82,15 +72,60 @@ class BusinessProfileTests: XCTestCase {
         objectModel.managedObjectContext.reset()
     }
 
+    /**
+    Check to see if the mimimal set of fields is detected correctly for non-USA/UAS countries
+    */
     func testGBRIsFilled() {
+        // Create a profile
+        var gbrBusinessProfile = BusinessProfile.insertInManagedObjectContext(objectModel.managedObjectContext) as! BusinessProfile
         
-        var gbrBusinessProfile = MockGBRBusinessProfile.insertInManagedObjectContext(objectModel.managedObjectContext)
-        
+        // Check to see if actually created
         XCTAssertNotNil(gbrBusinessProfile, "Couldn't create business profile")
+        
+        // Initialise it with UK specific data, and see if it is reported as filled correctly (for GBR)
+        gbrBusinessProfile.setupAsGBR()
         XCTAssertTrue(gbrBusinessProfile.isFilled(), "GBR Profile not completly filled")
         
-//        gbrBusinessProfile.city = ""
-        
+        // Nil out one of the attributes that are required, and see if it is now reported as not filled
+        gbrBusinessProfile.city = nil
         XCTAssertFalse(gbrBusinessProfile.isFilled(), "GBR Profile incorrectly reported as filled")
+    }
+    
+    /**
+    Check to see if all USA specific fields are detected
+    */
+    func testUSAIsFilled() {
+        // Create a profile
+        var usaBusinessProfile = BusinessProfile.insertInManagedObjectContext(objectModel.managedObjectContext) as! BusinessProfile
+        
+        // Check to see if actually created
+        XCTAssertNotNil(usaBusinessProfile, "Couldn't create business profile")
+        
+        // Initialise it with UK specific data, and see if it is reported as filled correctly (for GBR)
+        usaBusinessProfile.setupAsUSA()
+        XCTAssertTrue(usaBusinessProfile.isFilled(), "USA Profile not completly filled")
+        
+        // Nil out one of the attributes that are required, and see if it is now reported as not filled
+        usaBusinessProfile.city = nil
+        XCTAssertFalse(usaBusinessProfile.isFilled(), "USA Profile incorrectly reported as filled")
+    }
+    
+    /**
+    Check to see if all AUS specific fields are detected
+    */
+    func testAUSIsFilled() {
+        // Create a profile
+        var ausBusinessProfile = BusinessProfile.insertInManagedObjectContext(objectModel.managedObjectContext) as! BusinessProfile
+        
+        // Check to see if actually created
+        XCTAssertNotNil(ausBusinessProfile, "Couldn't create business profile")
+        
+        // Initialise it with UK specific data, and see if it is reported as filled correctly (for GBR)
+        ausBusinessProfile.setupAsAUS()
+        XCTAssertTrue(ausBusinessProfile.isFilled(), "AUS Profile not completly filled")
+        
+        // Nil out one of the attributes that are required, and see if it is now reported as not filled
+        ausBusinessProfile.city = nil
+        XCTAssertFalse(ausBusinessProfile.isFilled(), "AUS Profile incorrectly reported as filled")
     }
 }
