74d7a8737842ea1697c4512c9bec065d7dcbf6a3
Nick Banks <nick.banks@synchromation.com>
2015-06-29 17:37:29 +0100

    WIP - adding tests

--- TransferTests/BusinessProfileTests.swift
+++ TransferTests/BusinessProfileTests.swift
@@ -11,25 +11,61 @@ import XCTest
 
 class BusinessProfileTests: XCTestCase {
     
-    class MockBusinessProfile: BusinessProfile {
-
+    let objectModel = ObjectModel();
+    
+    // Set up some default constants for use in testing
+    class MockUSABusinessProfile : BusinessProfile {
+        
         override func awakeFromInsert() {
             super.awakeFromInsert()
             
-            abn = ""
-            acn = ""
-            addressFirstLine = ""
-            arbn = ""
+            addressFirstLine = "1708 Oakland Road, Suite 500"
             businessDescription = ""
-            city = ""
+            city = "San Jose"
             companyRole = ""
             companyType = ""
-            countryCode = ""
-            name = ""
-            postCode = ""
-            readonlyFields = ""
-            registrationNumber = ""
-            state = ""
+            countryCode = "USA"
+            name = "Dave Spart"
+            postCode = "95131"
+            registrationNumber = "07209813"
+            state = "California"
+        }
+    }
+    
+    class MockAUSBusinessProfile : BusinessProfile {
+        
+        override func awakeFromInsert() {
+            super.awakeFromInsert()
+            
+            abn = "121181976"
+            acn = "0123454321"
+            addressFirstLine = " 50 Hunter St,"
+            arbn = ""
+            businessDescription = ""
+            city = "Sydney"
+            companyRole = ""
+            companyType = ""
+            countryCode = "AUS"
+            name = "Dave Spart"
+            postCode = "2000"
+            registrationNumber = "07209813"
+        }
+    }
+    
+    class MockGBRBusinessProfile : BusinessProfile {
+        
+        override func awakeFromInsert() {
+            super.awakeFromInsert()
+            
+            addressFirstLine = "186-188 City Road"
+            businessDescription = "x"
+            city = "London"
+            companyRole = "x"
+            companyType = "x"
+            countryCode = "GBR"
+            name = "Dave Spart"
+            postCode = "EC1V 2NT"
+            registrationNumber = "07209813"
         }
     }
 
@@ -41,9 +77,20 @@ class BusinessProfileTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        
+        // Clear out any objects we might have added to our MOC (we haven't saved so don't need to cycle through the objects)
+        objectModel.managedObjectContext.reset()
     }
 
-    func testIsFilled() {
-
+    func testGBRIsFilled() {
+        
+        var gbrBusinessProfile = MockGBRBusinessProfile.insertInManagedObjectContext(objectModel.managedObjectContext)
+        
+        XCTAssertNotNil(gbrBusinessProfile, "Couldn't create business profile")
+        XCTAssertTrue(gbrBusinessProfile.isFilled(), "GBR Profile not completly filled")
+        
+//        gbrBusinessProfile.city = ""
+        
+        XCTAssertFalse(gbrBusinessProfile.isFilled(), "GBR Profile incorrectly reported as filled")
     }
 }
